a9d7f0430bb5d9d46a4656f28a3ca9a17f72f4b3
Sotiris Liagas <liagason@gmail.com>
2022-02-13 14:09:03 +0200

    upd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
     content: ["**/*.{html,js}"],
     theme: {
-      extend: {},
+      fontFamily: {
+        'sans': ['"Segoe UI"', 'system-ui'],
+        'serif': ['ui-serif'],
+        'mono': ['Consolas', 'ui-monospace'],
+        'display': ['"Segoe UI"', 'system-ui'],
+        'body': ['"Segoe UI"', 'system-ui'],
+      }
     },
     plugins: [],
   }
